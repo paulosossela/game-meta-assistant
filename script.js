7cefc99f788a82f9767b506f656dbb07c51bdc0b
Paulo Sossela <pauloroberto.sossela@gmail.com>
2025-07-11 16:54:27 -0300

    fix api leak

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const markdownToHTML = (text) => {
   return converter.makeHtml(text)
 }
 
-// AIzaSyC20yYsHfcdW4PfqdhsOlsGq6hIzZtYygs
 const perguntarAI = async (question, game, apiKey) => {
   const model = "gemini-2.5-flash"
   const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}` 
